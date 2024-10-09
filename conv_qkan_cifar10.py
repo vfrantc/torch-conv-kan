bb5382300747c23451a269892538f79458c6c898
Vladimir Frants <vladimirfrants@Vladimirs-MacBook-Air.local>
2024-10-09 11:34:36 -0400

    qkan

--- conv_qkan_cifar10.py
+++ conv_qkan_cifar10.py
@@ -33,9 +33,15 @@ def get_data():
 
 @hydra.main(version_base=None, config_path="./configs/", config_name="cafar10-qreskanet.yaml")
 def main(cfg):
-    model = resqkanet_18x32p(3, 10, groups=cfg.model.groups, degree=cfg.model.degree, width_scale=cfg.model.width_scale,
-                             dropout=cfg.model.dropout, l1_decay=cfg.model.l1_decay,
-                             dropout_linear=cfg.model.dropout_linear)
+    model = resqkanet_18x32p(3,
+                            10,
+                            groups=cfg.model.groups,
+                            spline_order=int(cfg.model.spline_order),
+                            grid_size=int(cfg.model.grid_size),
+                            base_activation=nn.SiLU,
+                            dropout=cfg.model.dropout,
+                            l1_decay=cfg.model.l1_decay,
+                            dropout_linear=cfg.model.dropout_linear)
     summary(model, (64, 3, 32, 32), device='cpu')
     dataset_train, dataset_test = get_data()
     loss_func = nn.CrossEntropyLoss(label_smoothing=cfg.loss.label_smoothing)
